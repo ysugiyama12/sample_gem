888f645c0cc6c5e32751abac6ecd3d31e137a099
ysugiyama12 <ysugiyama.1239@gmail.com>
2021-06-18 00:28:29 +0900

    initial commit

---
+++ lib/ys_first_gem.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "ys_first_gem/version"
+
+module YsFirstGem
+  class Error < StandardError; end
+  # Your code goes here...
+  def self.greet
+    "hello"
+  end
+
+end
---
+++ spec/ys_first_gem_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe YsFirstGem do
+  it "has a version number" do
+    expect(YsFirstGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
